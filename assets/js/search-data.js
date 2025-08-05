a2d26e95e0817aae393fb13406e762715cd7f40e
qingwanceceliacheng <cecelia.qw.cheng@gmail.com>
2025-08-05 04:33:47 +0000

    Deploying to gh-pages from @ qingwanceceliacheng/qingwanceceliacheng.github.io@aa1c98b66303133620f0a7169e0dacf1178fea12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,14 +456,14 @@ ninja.data = [{
           description: "An after-school program integrating social justice and technology for elementary school students",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-designing-a-better-course-evaluation-experience",
+          title: 'Designing a Better Course Evaluation Experience',
+          description: "A Web Platform to Support Constructive Communication Between Professors and Students.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-community-crates",
+          title: 'Community Crates',
+          description: "Creating a space for all generations to gather, leading to lives that are richer in meaning and purpose.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
